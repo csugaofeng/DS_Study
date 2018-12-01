807a446365407b1fc37b9036f13b1bf0593c55d7
PC_2 <csu13gaofeng@csu.edu.cn>
2018-12-01 23:31:25 +0800

    解决了模板类继承模板类，子类无法访问基类成员的问题

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -5,7 +5,12 @@ using std::endl;
 int main()
 {
 	Stack<int> Stack1;
+	Stack1.push(1);
+	Stack1.push(2);
+	Stack1.push(3);
+	Stack1.push(4);
 
+	cout << Stack1.top() << endl;
 	system(" pause ");
 	return 0;
 }
